282f9d4d0a6f0553f435be6a9a5da96e85e73f06
swatiitobuz <swati@itobuz.com>
2023-03-21 18:56:40 +0530

    done

--- module-weather-app/index.mjs
+++ module-weather-app/index.mjs
@@ -1,5 +1,5 @@
-import * as swati from './math.mjs'
-console.log(swati.add(1,2));
-console.log(swati.sub(2,1));
-console.log(swati.multi(2,3));
-console.log(swati.div(6,3));
+import * as operations from './math.mjs'
+console.log(operations.add(1,2));
+console.log(operations.sub(2,1));
+console.log(operations.multi(2,3));
+console.log(operations.div(6,3));
